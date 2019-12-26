8c66765e9d22adfb189bcb114194f2a157343dfc
xushuanglu <xushuanglu@datahunter.cn>
2019-12-26 19:13:18 +0800

    fundebug平台代码测试

---
+++ src/main/java/fundebug/FundebugTest.java
@@ -0,0 +1,12 @@
+package fundebug;
+
+import com.fundebug.Fundebug;
+
+public class FundebugTest {
+
+	public static void main(String[] args) {
+		Fundebug fundebug = new Fundebug("5cdb836744074efd6338baa87a651379c3dc081e09d5ea9800ef544c9086035a");
+		fundebug.notify("TEST", "Hello, Fundebug!");
+		System.out.println("Hello, World");
+	}
+}
